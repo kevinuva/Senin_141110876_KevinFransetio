a27a57c53641f51d56767a001a4c77a00e4002da
user <user>
2016-11-05 22:21:41 +0700

    Latihan_5_1

---
+++ Latihan_5_1/Form2.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Latihan_5_1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Background Color");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Theme", new System.Windows.Forms.TreeNode[] {
+            treeNode3});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.panel_bg_color = new System.Windows.Forms.Panel();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.cmb_box_bg_color = new System.Windows.Forms.ComboBox();
+            this.lblBgColor = new System.Windows.Forms.Label();
+            this.panel_bg_color.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            treeNode3.Name = "bg_color";
+            treeNode3.Text = "Background Color";
+            treeNode4.Name = "theme";
+            treeNode4.Text = "Theme";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4});
+            this.treeView1.Size = new System.Drawing.Size(140, 310);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // panel_bg_color
+            // 
+            this.panel_bg_color.BackColor = System.Drawing.SystemColors.Window;
+            this.panel_bg_color.Controls.Add(this.btn_cancel);
+            this.panel_bg_color.Controls.Add(this.btn_ok);
+            this.panel_bg_color.Controls.Add(this.cmb_box_bg_color);
+            this.panel_bg_color.Controls.Add(this.lblBgColor);
+            this.panel_bg_color.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_bg_color.Location = new System.Drawing.Point(140, 0);
+            this.panel_bg_color.Name = "panel_bg_color";
+            this.panel_bg_color.Size = new System.Drawing.Size(418, 310);
+            this.panel_bg_color.TabIndex = 1;
+            this.panel_bg_color.Visible = false;
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(331, 275);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancel.TabIndex = 3;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(250, 275);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(75, 23);
+            this.btn_ok.TabIndex = 2;
+            this.btn_ok.Text = "Ok";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // cmb_box_bg_color
+            // 
+            this.cmb_box_bg_color.FormattingEnabled = true;
+            this.cmb_box_bg_color.Location = new System.Drawing.Point(196, 27);
+            this.cmb_box_bg_color.Name = "cmb_box_bg_color";
+            this.cmb_box_bg_color.Size = new System.Drawing.Size(121, 21);
+            this.cmb_box_bg_color.TabIndex = 1;
+            // 
+            // lblBgColor
+            // 
+            this.lblBgColor.AutoSize = true;
+            this.lblBgColor.Location = new System.Drawing.Point(86, 30);
+            this.lblBgColor.Name = "lblBgColor";
+            this.lblBgColor.Size = new System.Drawing.Size(92, 13);
+            this.lblBgColor.TabIndex = 0;
+            this.lblBgColor.Text = "Background Color";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 310);
+            this.Controls.Add(this.panel_bg_color);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form2";
+            this.Text = "Form_Editor";
+            this.Load += new System.EventHandler(this.form_editor_Load);
+            this.panel_bg_color.ResumeLayout(false);
+            this.panel_bg_color.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Panel panel_bg_color;
+        private System.Windows.Forms.ComboBox cmb_box_bg_color;
+        private System.Windows.Forms.Label lblBgColor;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Button btn_ok;
+
+    }
+}
---
+++ Latihan_5_1/Form2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using System.Reflection;
+
+namespace Latihan_5_1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void form_editor_Load(object sender, EventArgs e)
+        {
+            //referensi Denny Ho
+            Form1 frm = (Form1) MdiParent;
+            Color clr = new Color();
+            PropertyInfo[] colors = clr.GetType().GetProperties();
+            foreach (PropertyInfo color in colors)
+            {
+                if (color.PropertyType == typeof(System.Drawing.Color))
+                {
+                    cmb_box_bg_color.Items.Add(color.Name);
+                }
+            }
+            this.cmb_box_bg_color.DrawMode = DrawMode.OwnerDrawFixed;
+            this.cmb_box_bg_color.DrawItem += new DrawItemEventHandler(cmb_box_bg_color_DrawItem);
+            this.cmb_box_bg_color.Text = frm.getBackgroundColor();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (treeView1.SelectedNode.Text == "Background Color")
+            {
+                panel_bg_color.Show();
+            }
+        }
+
+        private void cmb_box_bg_color_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            // a dropdownlist may initially have no item selected, so skip the highlighting:
+            if (e.Index >= 0)
+            {
+                Graphics g = e.Graphics;
+                Brush brush = new SolidBrush(e.BackColor);
+                Brush tBrush = new SolidBrush(e.ForeColor);
+                g.FillRectangle(brush, e.Bounds);
+                string s = (string)this.cmb_box_bg_color.Items[e.Index].ToString();
+                SolidBrush b = new SolidBrush(Color.FromName(s));
+                // Draw a rectangle and fill it with the current color
+                // and add the name to the right of the color
+                e.Graphics.DrawRectangle(Pens.Black, 2, e.Bounds.Top + 1, 20, 11);
+                e.Graphics.FillRectangle(b, 3, e.Bounds.Top + 2, 19, 10);
+                e.Graphics.DrawString(s, this.Font, Brushes.Black, 25, e.Bounds.Top);
+                brush.Dispose();
+                tBrush.Dispose();
+            }
+            e.DrawFocusRectangle();
+        }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            Form1 frm = (Form1)MdiParent;
+            frm.setBackgroundColor(cmb_box_bg_color.Text);
+            Close();
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            Form1 frm = (Form1)MdiParent;
+            frm.setBackgroundColor(cmb_box_bg_color.Text);
+        }
+
+    }
+}
